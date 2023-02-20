2fd032a54c5ba95789064af49bb956b5ad8fb194
Vyacheslav <brevnov_vs.nw@uraltexis.ru>
2023-02-20 12:24:57 +0300

    feat: parametrs for ParametrAttribute

--- Src/Gedaq.DbConnection/Attributes/ParametrAttribute.cs
+++ Src/Gedaq.DbConnection/Attributes/ParametrAttribute.cs
@@ -13,7 +13,12 @@ namespace Gedaq.DbConnection.Attributes
             DbType dbType = DbType.Object,
             int size = -1,
             bool nullable = false,
-            ParameterDirection direction = ParameterDirection.Input
+            ParameterDirection direction = ParameterDirection.Input,
+            string sourceColumn = "",
+            bool sourceColumnNullMapping = false,
+            DataRowVersion sourceVersion = DataRowVersion.Current,
+            byte scale = 0,
+            byte precision = 0
             )
         {
         }
