790a9ef570f7bd50f8a8293c1a6a0c3124174b40
Vyacheslav <softstonedevelop@gmail.com>
2023-09-30 19:45:59 +0300

    feat:
    + Add new parameter 'AsPartInterface'
    
    https://github.com/SoftStoneDevelop/Gedaq/issues/50

--- Src/Gedaq.DbConnection/Attributes/QueryBatchAttribute.cs
+++ Src/Gedaq.DbConnection/Attributes/QueryBatchAttribute.cs
@@ -11,7 +11,8 @@ namespace Gedaq.DbConnection.Attributes
             QueryType queryType,
             MethodType methodType,
             AccessModifier accessModifier = AccessModifier.AsContainingClass,
-            AsyncResult asyncResultType = AsyncResult.ValueTask
+            AsyncResult asyncResultType = AsyncResult.ValueTask,
+            Type asPartInterface = null
             )
         {
         }
